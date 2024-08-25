d3ec40facc6f83fa6d6e4b3dd52d60e4f923322a
hyspxt <patrick.alfieri@studio.unibo.it>
2024-08-25 20:06:25 +0200

    graph and AL implementation

--- src/datastructure/graph/GraphAL.java
+++ src/datastructure/graph/GraphAL.java
@@ -7,48 +7,89 @@ import java.util.Iterator;
 
 /**
  * Implementation of graphs using adjacent lists
+ * 
  * @param <D> type of the data object in the graph vertexes
- */	
+ */
 public class GraphAL<D> implements Graph<D> {
 
 	/** number of vertexes */
 	protected int n;
-	
+
 	/** number of edges */
 	protected int m;
-	
+
 	/** list of vertexes */
 	protected ArrayList<VertexAL<D>> vertexes;
 
 	/**
-   	 * Constructs a graph with implementation using adjacent lists
-	 */		
-	public GraphAL(){
+	 * Constructs a graph with implementation using adjacent lists
+	 */
+	public GraphAL() {
 		n = 0;
 		m = 0;
 		vertexes = new ArrayList<VertexAL<D>>();
 	}
 
-	public int vertexNum() { }
+	public int vertexNum() {
+		return this.n;
+	}
 
-	public int edgeNum() { }
+	public int edgeNum() {
+		return this.m;
+	}
 
-	public ArrayList<Vertex<D>> vertexes() { }
-	
-	public ArrayList<Edge<D>> edges() {	}
+	public ArrayList<Vertex<D>> vertexes() {
+		ArrayList<Vertex<D>> v = new ArrayList<Vertex<D>>();
+		for (int i = 0; i < n; i++)
+			v.add(vertex(i));
+		return v;
+	}
 
-	public int outDegree(Vertex<D> v) {	}
-	
-	public ArrayList<Edge<D>> outEdges(Vertex<D> v) { }
-	
-	public Edge<D> areAdjacent(Vertex<D> x, Vertex<D> y) { }
+	public ArrayList<Edge<D>> edges() {
+		ArrayList<Edge<D>> l = new ArrayList<Edge<D>>();
+		for (Vertex<D> v : vertexes()) {
+			for (Edge<D> e : ((VertexAL<D>) v).adjac) {
+				l.add(e);
+			}
+		}
+		return l;
+	}
 
-	public Vertex<D> addVertex(D data) { }
+	public int outDegree(Vertex<D> v) {
+		return ((VertexAL<D>) v).adjac.size();
+	}
 
-	public void addEdge(Edge<D> e) { }
-	
+	public ArrayList<Edge<D>> outEdges(Vertex<D> v) {
+		ArrayList<Edge<D>> l = new ArrayList<Edge<D>>();
+		for (Edge<D> e : ((VertexAL<D>) v).adjac) {
+			l.add(e);
+		}
+		return l;
+	}
+
+	public Edge<D> areAdjacent(Vertex<D> x, Vertex<D> y) {
+		for (Edge<D> e : outEdges(x)) {
+			if (e.dest == y)
+				return e;
+		}
+		return null;
+	}
+
+	public Vertex<D> addVertex(D data) {
+		VertexAL<D> v = new VertexAL<D>(data, this.n);
+		vertexes.add(v);
+		this.n++;
+		return v;
+	}
+
+	public void addEdge(Edge<D> e) {
+		VertexAL<D> v = (VertexAL<D>) e.source;
+		v.adjac.add(e);
+		this.m++;
+	}
+
 	public void removeVertex(Vertex<D> v) {
-		VertexAL<D> vAL = (VertexAL<D>)v;
+		VertexAL<D> vAL = (VertexAL<D>) v;
 		m = m - vAL.adjac.size();
 		n = n - 1;
 		if (vAL.index == n) {
@@ -56,40 +97,49 @@ public class GraphAL<D> implements Graph<D> {
 		} else {
 			VertexAL<D> vert = vertexes.remove(n);
 			vert.index = vAL.index;
-			vertexes.set(vert.index,vert);
+			vertexes.set(vert.index, vert);
 		}
 		Edge<D> e;
-		for (int i=0; i<vertexes.size(); i++) {
+		for (int i = 0; i < vertexes.size(); i++) {
 			Iterator<Edge<D>> it = (vertexes.get(i)).adjac.iterator();
-			while (it.hasNext()) { 
+			while (it.hasNext()) {
 				e = it.next();
 				if (e.dest == v) {
 					it.remove();
 					m = m - 1;
 				}
-			}					
+			}
 		}
 	}
 
-	public void removeEdge(Edge<D> e) { }
+	public void removeEdge(Edge<D> e) {
+		VertexAL<D> v = (VertexAL<D>)e.source;
+		if(v.adjac.contains(e)) {
+			v.adjac.remove(e);
+			m--;
+		}
+	}
 
 	/**
-	 *  Returns the index of a vertex
-     *  @param v vertex
-	 *  @return the index of the vertex v
-	 */	
+	 * Returns the index of a vertex
+	 * 
+	 * @param v vertex
+	 * @return the index of the vertex v
+	 */
 	protected int index(VertexAL<D> v) {
 		return v.index;
 	}
 
 	/**
-	 *  Returns the vertex with a given index
-     *  @param i the index
-	 *  @return the vertex v with index i
-	 */		
- 	protected VertexAL<D> vertex(int i) {
-		if (i < 0 || i >= n) return null;
+	 * Returns the vertex with a given index
+	 * 
+	 * @param i the index
+	 * @return the vertex v with index i
+	 */
+	protected VertexAL<D> vertex(int i) {
+		if (i < 0 || i >= n)
+			return null;
 		return vertexes.get(i);
- 	}
+	}
 
 }
